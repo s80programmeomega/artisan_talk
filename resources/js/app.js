2af1e66a47e5d9b55beb1558eb2b27b09926afe0
Jonas Levis <s80programmeomega@gmail.com>
2025-10-12 01:35:53 +0100

    Added authentication and congigured reverb.

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,9 @@
 import './bootstrap';
+
+
+// Disable this alpine to avoid any duplicate loading together with alpine from livewire
+import Alpine from 'alpinejs';
+
+window.Alpine = Alpine;
+
+Alpine.start();
